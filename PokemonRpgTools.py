d3fbb770b000d0b447f47b110c6f31aacabb39b2
Nolan Melander <melander.nolan@gmail.com>
2019-02-18 22:29:26 -0700

    Updated tools menu

--- PokemonRpgTools.py
+++ PokemonRpgTools.py
@@ -4,23 +4,27 @@ import TrainerCardCreator as trainer
 
 def pokemonrpgtools():
     version = "VERSION 0.0.1 - ALPHA\n"
-    print("Pokemon Rpg Tools".center(60, "-"))
-    print("Copywrite 2019 | All Rights Reserved".center(60, " "))
-    print(version.center(60, " "))
+    print("Pokemon Rpg Tools".center(120, "-"))
+    print("Copyright 2019 | All Rights Reserved".center(120, " "))
+    print(version.center(120, " "))
 
     choice = None
     while choice is not 'q':
         print("\n")
-        print("Trainer Card Creator (Currently Unavailable)", "(t)".rjust(22, "."))
-        print("Kanto Region (Currently Unavailable)", "(r)".rjust(30, "."))
-        print("Quit Pokemon Rpg Tools", "(q)".rjust(20, "."))
+        print("Trainer Card Creator (Currently Unavailable)", "(-)".rjust(22, "."))
+        print("Kanto Region (Currently Unavailable)", "(-)".rjust(30, "."))
+        print("Pokemon Stat Generator (Currently Unavailable)", "(-)".rjust(20, "."))
+        print("Quit Pokemon Rpg Tools", "(q)".rjust(44, "."))
         choice = input("\nPlease pick a program to load: ")
 
-        if choice.lower() is 't':
+        if choice is 't':
+            assert choice is 't'
             trainer.trainercardcreator()
-        elif choice.lower() is 'r':
+        elif choice is 'k':
+            assert choice is 'k'
             kanto.kantoroutecreator()
-        else:
+        elif choice is not 'q':
+            assert choice is not 'q'
             print("Invalid input. Please try again.")
 
 
