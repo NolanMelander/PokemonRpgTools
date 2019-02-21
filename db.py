5634711a951af12f4924bf9894664e4734e8c4bb
Nolan Melander <melander.nolan@gmail.com>
2019-02-21 16:40:03 -0700

    Added another function for pulling from database

--- db.py
+++ db.py
@@ -1,10 +1,24 @@
 import quantumrandom as qr
 import connect as c
 
-def getStats():
+
+def getnature():
+    connection = c.connect()
+    random = int(qr.randint(0, 24))
+    cursor = connection.cursor()
+    sql = 'Select * from Natures where Nature=%s'
+    cursor.execute(sql, (random,))
+    rec = cursor.fetchall()
+
+    if connection.is_connected():
+        connection.close()
+
+    return rec
+
+
+def getstats():
     connection = c.connect()
     random = int(qr.randint(1,150))
-    db_info = connection.get_server_info()
     cursor = connection.cursor()
     sql = 'Select * from Stats where `National Dex`=%s'
     cursor.execute(sql, (random,))
