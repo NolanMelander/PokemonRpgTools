656e7e1cf0d259fbd3eaceb1e922d74bfbabcc53
Nolan Melander <melander.nolan@gmail.com>
2019-02-21 16:40:55 -0700

    Added several new functions for generating the pokemon

--- PokemonStatGenerator.py
+++ PokemonStatGenerator.py
@@ -1,18 +1,38 @@
 import os
+from math import floor
+
 import db
 import quantumrandom as qr
 
 
-def display():
+def display(nature, iv, stats, pokemon, pokedex, lvl):
+    print("Pokemon: ", pokemon)
+    print("National Pokedex: ", pokedex)
+    print("Level: ", lvl)
+    print("HP: ", stats[0])
+    print("POKEMON IV")
+    print("HP: ", iv[0])
+    print("Attack: ", iv[1])
+    print("Defense: ", iv[2])
+    print("Spa: ", iv[3])
+    print("Spd: ", iv[4])
+    print("Speed: ", iv[5])
+
+
+def gethp(base, iv, ev, lvl):
+    hp = floor(((2*base) + iv + floor(ev/4))/100) + lvl + 10
+    return hp
+
+
+def getstats(base, nature, iv, ev):
     pass
 
-
 def getiv():
-    base = [0, 0, 0, 0, 0, 0]
+    iv = [0, 0, 0, 0, 0, 0]
     for x in range(6):
-        base[x] = int(qr.randint(0, 35))
+        iv[x] = int(qr.randint(0, 35))
 
-    return base
+    return iv
 
 
 def pokemonstatsgenerator():
@@ -22,21 +42,42 @@ def pokemonstatsgenerator():
     print(version.center(120, " "))
     print("\n")
 
-    pokemon = pokedex = hp = attack = defense = spa = spd = speed = None
+    ev = [0, 0, 0, 0, 0, 0]
+    lvl = input("Please enter desired pokemon level:")
 
-    base = db.getStats()
 
+    print("Generating pokemon. This may take a few minutes. Please wait...")
+
+    base = db.getstats()
+
+    pokemon = None
+    pokedex = None
+    basestat = [0, 0, 0, 0, 0, 0]
     for row in base:
         pokemon = (row[0])
         pokedex = (row[1])
-        hp = (row[2])
-        attack =(row[3])
-        defense =(row[4])
-        spa =(row[5])
-        spd =(row[6])
-        speed =(row[7])
+        basestat[0] = (row[2])
+        basestat[1] = (row[3])
+        basestat[2] = (row[4])
+        basestat[3] = (row[5])
+        basestat[4] = (row[6])
+        basestat[5] = (row[7])
 
-    base = getiv()
+    nature = db.getnature()
 
+    basenat = [0, 0, 0, 0, 0]
+    for row in nature:
+        nature = (row[0])
+        basenat[0] = (row[2])
+        basenat[1] = (row[3])
+        basenat[2] = (row[4])
+        basenat[3] = (row[5])
+        basenat[4] = (row[6])
+    iv = getiv()
+
+    stats = [0, 0, 0, 0, 0, 0]
+
+    stats[0] = gethp(basestat[0], iv[0], ev[0], int(lvl))
+    display(nature, iv, stats, pokemon, pokedex, lvl)
     os.system("pause")
 
