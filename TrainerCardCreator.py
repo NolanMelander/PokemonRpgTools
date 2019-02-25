cf821539b4f733ae12e1e1b82020d964062aba0b
Nolan Melander <melander.nolan@gmail.com>
2019-02-25 13:59:42 -0700

    Added display function. Reworked several others

--- TrainerCardCreator.py
+++ TrainerCardCreator.py
@@ -1,6 +1,25 @@
 import os
 import quantumrandom as qr
+import db
 
+
+def display(tnumber, tname, pokemon):
+    print("Trainer Card".center(120, "-"))
+    print("Trainer Name: ", tname)
+    print("Trainer Number: ", tnumber)
+    print("".rjust(30, "-"))
+    print("Option 1: ", pokemon[0])
+    print("Option 2: ", pokemon[1])
+    print("Option 3: ", pokemon[2])
+    print("".rjust(30, "-"))
+
+def getpokemon():
+    pokemon = ["", "", ""]
+    for x in range(3):
+        options = db.getstats()
+        for rows in options:
+            pokemon[x] = (rows[0])
+    return pokemon
 
 def registertrainer():
     trainer_name = input("What is the name of the trainer?")
@@ -10,12 +29,12 @@ def registertrainer():
     trainer_number = int(qr.randint(111111111111, 999999999999))
 
     print(trainer_name, "registered successfully.")
-    print("Trainer Number: ", trainer_number)
-    pass
+
+    return trainer_number, trainer_name
 
 
 def trainercardcreator():
-    version = "VERSION 0.0.1 - ALPHA\n"
+    version = "VERSION 0.1.0 - ALPHA\n"
     print("Trainer Card Creator".center(120, "-"))
     print("Copyright 2019 | All Rights Reserved".center(120, " "))
     print(version.center(120, " "))
@@ -25,9 +44,14 @@ def trainercardcreator():
     if choice is 'y':
         assert choice is 'y'
         print("Registering new trainer...")
-        registertrainer()
+        tnumber, tname = registertrainer()
+        print("Checking available pokemon...")
+        pokemon = getpokemon()
+        print("Pokemon Found.")
+        display(tnumber, tname, pokemon)
     elif choice is 'n':
         assert choice is 'n'
         print("Returning to Pokemon Rpg Tools menu...")
 
     os.system("pause")
+
