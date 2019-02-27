33da84414ac4989eaa9eca34512542063ffae691
Nolan Melander <melander.nolan@gmail.com>
2019-02-27 14:30:27 -0700

    Fixed several prompts and added new prompts for pokemon iv.

--- PokemonStatGenerator.py
+++ PokemonStatGenerator.py
@@ -26,7 +26,6 @@ def display(nature, iv, stats, pokemon, pokedex, lvl):
     print("Speed: ", iv[5])
 
 
-
 def gethp(base, iv, ev, lvl):
     hp = floor(((2*base) + iv + floor(ev/4))/100) + lvl + 10
     return hp
@@ -54,14 +53,28 @@ def pokemonstatsgenerator():
 
     choice = None
     natdex = None
-    choice = input("Randomly generate pokemon (y/n)? ")
+    choice = input("Do you wish to generate a specific pokemon (y/n)? ")
 
     if choice is 'y':
         natdex = input("Please input the National Pokedex number of Pokemon: ")
+        choice = input("Do you wish to input iv values? (y/n)")
+    else:
+        choice = None
+
+    if choice is 'y':
+        iv = [0, 0, 0, 0, 0, 0]
+        iv[0] = input("Please input the iv value of hp: ")
+        iv[1] = input("Please input the iv value of attack: ")
+        iv[2] = input("Please input the iv value of defense: ")
+        iv[3] = input("Please input the iv value of spa: ")
+        iv[4] = input("Please input the iv value of spd: ")
+        iv[5] = input("Please input the iv value of speed: ")
+
+    else:
+        iv = getiv()
 
     ev = [0, 0, 0, 0, 0, 0]
-    lvl = input("Please enter desired pokemon level:")
-
+    lvl = input("Please enter desired pokemon level: ")
 
     print("Generating pokemon. This may take a few minutes. Please wait...")
 
@@ -96,18 +109,13 @@ def pokemonstatsgenerator():
         basenat[3] = (row[5])
         basenat[4] = (row[6])
 
-
-
-    iv = getiv()
-
     stats = [0, 0, 0, 0, 0, 0]
-    stats[0] = gethp(basestat[0], iv[0], ev[0], int(lvl))
-
-    stats[1] = getstats(basestat[1], basenat[0], iv[1], ev[1], int(lvl))
-    stats[2] = getstats(basestat[2], basenat[1], iv[2], ev[2], int(lvl))
-    stats[3] = getstats(basestat[3], basenat[2], iv[3], ev[3], int(lvl))
-    stats[4] = getstats(basestat[4], basenat[3], iv[4], ev[4], int(lvl))
-    stats[5] = getstats(basestat[5], basenat[4], iv[5], ev[5], int(lvl))
+    stats[0] = gethp(basestat[0], int(iv[0]), ev[0], int(lvl))
+    stats[1] = getstats(basestat[1], basenat[0], int(iv[1]), ev[1], int(lvl))
+    stats[2] = getstats(basestat[2], basenat[1], int(iv[2]), ev[2], int(lvl))
+    stats[3] = getstats(basestat[3], basenat[2], int(iv[3]), ev[3], int(lvl))
+    stats[4] = getstats(basestat[4], basenat[3], int(iv[4]), ev[4], int(lvl))
+    stats[5] = getstats(basestat[5], basenat[4], int(iv[5]), ev[5], int(lvl))
 
     display(nat, iv, stats, pokemon, pokedex, int(lvl))
     os.system("pause")
